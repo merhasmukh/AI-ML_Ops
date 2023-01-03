aa51993683d59a67f027b61f4ae4806ec3c2b8f6
mhgn <mhgn0001@gmail.com>
2023-01-03 17:11:58 +0530

    simple_dvc_project structure added

--- simple_dvc_project/setup.py
+++ simple_dvc_project/setup.py
@@ -11,5 +11,15 @@ setup(
     author="hasmukh",
     description="A small package for DVC ML pipeline demo"
     long_description=long_description,
-    author_email="mhgn0001@gmail.com"
+    url="https://github.com/mhgn/AI-ML_Ops/tree/master/simple_dvc_project",
+    author_email="mhgn0001@gmail.com",
+    package_dir={"":"src"},
+    packages=find_packages(where="src"),
+    python_requires=">=3.6",
+    install_requires=[
+        'dvc',
+        'dvc[gdrive]',
+        'pandas',
+        'scikit-learn'
+    ]
 )
